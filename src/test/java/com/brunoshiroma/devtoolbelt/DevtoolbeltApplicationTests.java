362c25b07911b3bbef44b57d0c4d0e145fc07d82
Bruno Shiroma <brunoshiroma@gmail.com>
2020-01-23 11:10:47 -0300

    initial commit

---
+++ src/test/java/com/brunoshiroma/devtoolbelt/DevtoolbeltApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunoshiroma.devtoolbelt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevtoolbeltApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
