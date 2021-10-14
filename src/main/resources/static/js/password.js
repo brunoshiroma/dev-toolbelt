aec54bdea88eeb57f0bd25dd6ca0a97c0ec1f264
Bruno Shiroma <brunoshiroma@gmail.com>
2021-10-13 21:01:58 -0300

    split special char on password, common and uncommon

--- src/main/resources/static/js/password.js
+++ src/main/resources/static/js/password.js
@@ -1,10 +1,11 @@
 const __UPPER =["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const __LOWER = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const __NUMBER = ["0","1","2","3","4","5","6","7","8","9"];
-const __SPECIAL = ["!","#", "$", "%", "?", "&", "*", "(", ")", "<", ">", ",", ".", ";","[", "]", "-", "+", "=","\\", "|"];
+const __SPECIAL_COMMON = ["!","#", "$", "%", "?", "&", "*"];
+const __SPECIAL_UNCOMMON = ["(", ")", "<", ">", ",", ".", ";","[", "]", "-", "+", "=","\\", "|"];
 
 
-function initCharArray(useUpper, useLower, useNumber, useSpecial){
+function initCharArray(useUpper, useLower, useNumber, useSpecialCommon, useSpecialUncommon){
     let chars = [];
 
     if(useUpper){
@@ -16,16 +17,19 @@ function initCharArray(useUpper, useLower, useNumber, useSpecial){
     if(useNumber){
         chars = chars.concat(__NUMBER);
     }
-    if(useSpecial){
-        chars = chars.concat(__SPECIAL);
+    if(useSpecialCommon){
+        chars = chars.concat(__SPECIAL_COMMON);
+    }
+    if(useSpecialUncommon){
+        chars = chars.concat(__SPECIAL_UNCOMMON)
     }
 
     return chars;
 }
 
-async function generatePass(useUpper, useLower, useNumber, useSpecial, size) {
+async function generatePass(useUpper, useLower, useNumber, useSpecialCommon, useSpecialUncommon, size) {
 
-    const charsArray = initCharArray(useUpper, useLower, useNumber, useSpecial);
+    const charsArray = initCharArray(useUpper, useLower, useNumber, useSpecialCommon, useSpecialUncommon);
 
     const pass = new Array(size);
 
